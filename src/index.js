be4ff08132c43f2f1228a5143d16e2922006fbc9
B1NK* <62927093+fxxk3rrth4ng@users.noreply.github.com>
2025-08-17 23:51:12 +0900

    Update ids

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ export default {
       '0f171fd568c130cf09f2d3a641c4cbf1', // 김편집
       'de33a533e705c56ebbe8f087d609ce48', // 현성팍
       // 'c663e425e015731a5665a57fbac30bb6', // 라모
-      '6e06f5e1907f17eff543abd06cb62891', // 녹두로
+      // '6e06f5e1907f17eff543abd06cb62891', // 녹두로
       // '7d4157ae4fddab134243704cab847f23', // 펀즈이누
-      'b8119eaadca58a5cd0c8cf2d48af8bea', // 일급천재
+      // 'b8119eaadca58a5cd0c8cf2d48af8bea', // 일급천재
       '0889909c5e4a8234a1f963cf4a9e3081', // 칸나비
       '96fe5087cb0195df09ad54cac8a1e24a', // 서치
-      '61ca8b9ffd80dc8e529148734f9352da', // 무츠키쨩
-      '798e100206987b59805cfb75f927e965', // 디디디용
+      // '61ca8b9ffd80dc8e529148734f9352da', // 무츠키쨩
+      // '798e100206987b59805cfb75f927e965', // 디디디용
     ]
 
     for (let id of ids) {
